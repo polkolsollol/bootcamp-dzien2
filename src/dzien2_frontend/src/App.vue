a52afd1f68f449a85e72c0381c06abe568abb590
polkolsollol <kuba.bochenski@vp.pl>
2024-06-26 19:07:37 +0200

    added styles

--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -1,21 +1,12 @@
 <script setup>
 import Blog from './components/Blog.vue';
-
 </script>
 
 <template>
-  <main>
+  <main class="container mx-auto">
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <h1 class="text-blue-600">Siema?!</h1>
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <input id="numer" alt="Numer" type="number" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
     <Blog />
   </main>
 </template>
