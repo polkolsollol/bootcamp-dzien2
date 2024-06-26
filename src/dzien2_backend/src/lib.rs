d9718418661070c2a1a490a5c18e0bf8b02f2b66
polkolsollol <kuba.bochenski@vp.pl>
2024-06-26 18:46:31 +0200

    added tailwind

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -24,18 +24,18 @@ fn odczytaj_wpisy() -> Vec<String> {
 }
 
 #[ic_cdk::update]
-fn usun_wpis(id_wpisu: usize) {
-    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+fn usun_wpis(id_wpisu: usize){
+    WPISY.with(|wpisy| {
         wpisy.borrow_mut().remove(id_wpisu)
     });
 }
 
 #[ic_cdk::update]
-fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String) {
-    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String){
+    WPISY.with(|wpisy| {
         let mut binding = wpisy.borrow_mut();
         let wpis = binding.get_mut(id_wpisu);
         let stary_wpis = wpis.unwrap();
         *stary_wpis = nowy_wpis;
-    });
-} 
+    });  
+}
--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -1,18 +1,6 @@
 <script setup>
-import Blog from "./components/Blog.vue"
-import { ref } from 'vue';
-import { dzien2_backend } from 'declarations/dzien2_backend/index';
-let greeting = ref('');
+import Blog from './components/Blog.vue';
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  const number = target.querySelector('#number').value;
-  await dzien2_backend.greet(name, Number(number)).then((response) => {
-    greeting.value = response;
-  });
-}
 </script>
 
 <template>
@@ -20,10 +8,11 @@ async function handleSubmit(e) {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+    <h1 class="text-blue-600">Siema?!</h1>
     <form action="#" @submit="handleSubmit">
       <label for="name">Enter your name: &nbsp;</label>
       <input id="name" alt="Name" type="text" />
-      <input id="number" alt="Number" type="number" />
+      <input id="numer" alt="Numer" type="number" />
       <button type="submit">Click Me!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
